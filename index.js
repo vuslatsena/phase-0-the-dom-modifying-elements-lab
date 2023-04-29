b427e041c72d02f81b16064ca2eb52d8b51b527f
vuslatsena <vuslatsena23@gmail.com>
2023-04-29 08:34:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const removingMain = document.getElementById("main");
+removingMain.remove();
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+
+newHeader.id = "victory";
+newHeader.textContent = "Vuslat is the champion";
